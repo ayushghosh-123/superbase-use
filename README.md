017cd499c533c11d8f4f290c3c11ea4e2df0990c
Ayush ghosh <ghoshayush910@gmail.com>
2025-02-28 23:35:39 +0530

    readme update for clear idea of this repositry .

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Superbase Basic Project 🔥
 ----
+
+Hello every one this repositry i practice authentication with superbase as a backend service
+
+----
+## Tech stack 👍
+
+a taiwind 
+b React 
+c Superbase 
+
+----
+## workflow - 
+
+Bassically I  understand tgithye routng and authentication process through the small pr0ojecty .
+I make only signup , login , logout pages four my hackathon project.
+
+---
+## How to use4 it -
+
+1. git clone - 
+
+```gitclone
+
+git clone https://github.com/ayushghosh-123/superbase-use.git
+
+```
+
+2. Run npm dev - 
+
+```nodejs
+
+npm run dev
+
+```
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements} from 'react-router-dom';
-import App from './App.jsx';
 import Signup from './Component/Signup.jsx';
 import Signin from './Component/Signin.jsx';
 import Dashboard from './Component/Dashbord.jsx'; // Ensure correct spelling (Dashboard vs Dashbord)
@@ -11,7 +10,6 @@ import { AuthContextProvider } from './assets/Context/AuthContext.jsx';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route path="/" element={<App />} />
       <Route path="/signup" element={<Signup/>} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/dashboard" element={<Dashboard />} /> 
